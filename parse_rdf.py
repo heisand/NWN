fbde6185b2d35dc96e7e13d3fc640f9564610504
heisand <heisand_p@hotmail.com>
2016-05-22 20:44:03 +0200

    Update parse_rdf.py

--- parse_rdf.py
+++ parse_rdf.py
@@ -1,9 +1,14 @@
 # coding=utf8
 
+import sys
+file1 = sys.argv[1] # synsets.rdf
+file2 = sys.argv[2] # wordsenses.rdf
+file3 = sys.argv[3] # words.rdf
+file4 = sys.argv[4] # hyponymOf.rdf
+
 import rdflib
 from rdflib import URIRef
 from rdflib.namespace import RDF
-import os
 
 synsets = rdflib.Graph()
 wordsenses = rdflib.Graph()
@@ -11,27 +16,30 @@ words = rdflib.Graph()
 hyponymy = rdflib.Graph()
 wordsenses = rdflib.Graph()
 
-synsets.parse("reduced_synsets.rdf")
-wordsenses.parse("reduced_wordsenses2.rdf")
-words.parse("reduced_words2.rdf")
-hyponymy.parse("reduced_hyponymOf.rdf")
-synsets.parse("reduced_hyponymOf.rdf") # merging synsets and hyponymOf     
+synsets.parse(file1)
+wordsenses.parse(file2)
+words.parse(file3)
+hyponymy.parse(file4)
+synsets.parse(file4) # merging synsets and hyponymOf     
+
+# Some useful URIs.
 
 hyp = URIRef("http://www.wordnet.dk/owl/instance/2009/03/schema/hyponymOf")
-
 word = "http://www.w3.org/2006/03/wn/wn20/schema/word"
 wordsense = "http://www.w3.org/2006/03/wn/wn20/schema/containsWordSense"
 lexform = "http://www.w3.org/2006/03/wn/wn20/schema/lexicalForm"
 nounURI = "http://www.w3.org/2006/03/wn/wn20/schema/NounWordSense"
 verbURI = "http://www.w3.org/2006/03/wn/wn20/schema/VerbWordSense"
 adjURI = "http://www.w3.org/2006/03/wn/wn20/schema/AdjectiveWordSense"
-
 lexform = "http://www.w3.org/2006/03/wn/wn20/schema/lexicalForm"
 word = "http://www.w3.org/2006/03/wn/wn20/schema/word"
 
 lookup_id = {}
 lookup_string ={}
 unique_lex = set()
+
+# Iterating over the words and their words and their wordsenses, 
+# and map word IDs to the lexical forms and vice versa.
 
 for s, p, o in words.triples((None, URIRef(lexform), None)):
 	for s2, p2, o2 in wordsenses.triples((None, URIRef(word), s)):
